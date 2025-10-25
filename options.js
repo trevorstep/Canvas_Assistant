71f023b41d28f504628a1b712c48dc9261e3ccdf
Trevor Stephenson <trevors19themail@gmail.com>
2025-10-25 11:21:01 -0600

    Fixed save buttons

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
-document.getElementById('save').addEventListener('click', async () => {
+document.getElementById('saved').addEventListener('click', async () => {
   const token = document.getElementById('token').value.trim();
   if (!token) return alert('Paste token');
   await chrome.storage.sync.set({ canvasToken: token });
